ab3f330c3e5ba77867138c2fc222b30b9b246606
cuong Phan <blackangel1306@gmail.com>
2016-05-27 10:35:09 +0700

    Gem

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20160527020946) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "pullrequests", force: :cascade do |t|
     t.string   "username"
     t.string   "ipserver"
